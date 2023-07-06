451849a1058798d74aad4b092c1e50eabc43eb50
“Jakob <jnunnendorf@icloud.com>
2023-07-07 00:47:27 +0800

    test

--- src/app/week-1/elevator/codeBlock/CodeBlock.tsx
+++ src/app/week-1/elevator/codeBlock/CodeBlock.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 
 export default async function CodeBlock() {
-    const res = await fetch(`${process.env.VERCEL_URL}/api/txt-to-string`);
+    const res = await fetch(
+        `https://${process.env.VERCEL_URL}/api/txt-to-string`
+    );
     const pseudoCodeString = await res.json();
     return (
         <pre className="p-4 text-orange-300 rounded-md w-fit bg-slate-900">
