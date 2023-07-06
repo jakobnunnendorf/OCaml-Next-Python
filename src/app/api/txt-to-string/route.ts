662980b7031a7bb2ac21012378a478a7078721e2
“Jakob <jnunnendorf@icloud.com>
2023-07-07 01:09:34 +0800

    test

--- src/app/api/txt-to-string/route.ts
+++ src/app/api/txt-to-string/route.ts
@@ -1,9 +1,14 @@
 import { NextResponse } from "next/server";
 import fs from "fs";
+import path from "path";
 
 export async function GET(req: Request) {
     // // get filePath from request
-    const filePath = `https://${process.env.VERCEL_URL}/week-1/elevator/pseudoCode.txt`;
+    // const filePath = `https://${process.env.VERCEL_URL}/week-1/elevator/pseudoCode.txt`;
+    const filePath = path.join(
+        process.cwd(),
+        "src/app/week-1/elevator/pseudoCode.txt"
+    );
     // read file
     const txtAsString = fs.readFileSync(filePath, "utf8");
     // return file contents
