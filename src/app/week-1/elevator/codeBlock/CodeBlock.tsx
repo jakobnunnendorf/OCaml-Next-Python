a7caf5e7944d74696267dc7cc6f45018aadeee9e
“Jakob <jnunnendorf@icloud.com>
2023-07-07 00:37:37 +0800

    fix(codeblock): remove cache

--- src/app/week-1/elevator/codeBlock/CodeBlock.tsx
+++ src/app/week-1/elevator/codeBlock/CodeBlock.tsx
@@ -1,12 +1,7 @@
 import React from "react";
 
 export default async function CodeBlock() {
-    const res = await fetch(
-        `${process.env.NEXT_PUBLIC_URL}/api/txt-to-string`,
-        {
-            cache: "no-store",
-        }
-    );
+    const res = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/txt-to-string`);
     const pseudoCodeString = await res.json();
     return (
         <pre className="p-4 text-orange-300 rounded-md w-fit bg-slate-900">
