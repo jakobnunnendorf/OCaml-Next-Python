af64e819df2bbc7af74f134a5d4313571d22bd8c
“Jakob <jnunnendorf@icloud.com>
2023-08-09 15:04:39 +0800

    make the fibonacci function available
    Co-authored-by: IVectorI <IVectorI@users.noreply.github.com>

--- src/app/week-5/layout.js
+++ src/app/week-5/layout.js
@@ -1,3 +1,10 @@
-export default function Page() {
-    return <h1>Hello, Dashboard Page!</h1>;
+export default function Page({
+    children, // will be a page or nested layout
+}) {
+    return (
+        <section>
+            <h1 className="text-3xl">Week 5 - Javascript to React</h1>
+            {children}
+        </section>
+    );
 }
--- src/app/week-5/page.js
+++ src/app/week-5/page.js
@@ -1,5 +1,28 @@
+"use client";
+import React from "react";
 import fibonacci from "./fibonacci/fibonacci";
 
 export default function Page() {
-    return <pre>{fibonacci(10)}</pre>;
+    const [n, setN] = React.useState(0);
+    return (
+        <div>
+            <h2>Try our Fibonacci function generating numbers up to n</h2>
+            <label>
+                Enter number of elements:
+                <input
+                    onChange={(e) => setN(parseInt(e.target.value))}
+                    type="text"
+                    placeholder=""
+                    className="bg-slate-200"
+                />
+            </label>
+            <pre>
+                {fibonacci(n).map((element) => {
+                    return `${element}, `;
+                })}
+            </pre>
+        </div>
+    );
 }
+
+// yarn dev
