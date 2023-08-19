de7ca3e8ee97cc86e98a8376fbd3a7b3852ceb08
“Jakob <jnunnendorf@icloud.com>
2023-08-19 10:11:00 +0800

    feat(todos): add new todos to list
    Co-authored-by: IVectorI <IVectorI@users.noreply.github.com>

--- src/app/week-5/Wednesday910.js
+++ src/app/week-5/Wednesday910.js
@@ -1,48 +1,107 @@
 import React from "react";
+import { FiCheck } from "react-icons/fi";
 
 export default function Wednesday910() {
-    const [todos, setTodos] = React.useState(["Todo 1", "Todo 2", "Todo 3"]);
+    const todoObject = {
+        name: "Do the dishes",
+        dueDate: "2021-10-13",
+        status: "Not started",
+    };
+
+    const [todos, setTodos] = React.useState([todoObject]);
+    const [newTodoName, setNewTodoName] = React.useState([]);
+    const [newTodoStatus, setNewTodoStatus] = React.useState([]);
+    const [newTodoDate, setNewTodoDate] = React.useState([]);
 
     function removeTodo(todoList, todoThatShouldBeRemoved) {
-        // todoThatShouldBeRemoved is a string
-        // first find index:
-        const ToDoCounter = todoList.length;
-        let targetIndex;
-        let i = 0;
-        while (i < ToDoCounter) {
-            if (todoList[i] === todoThatShouldBeRemoved) {
-                targetIndex = i;
-            }
-            i++;
-        }
-        todoList.splice(targetIndex, 1);
+        const todoListWithoutTodoThatShouldBeRemoved = todoList.filter(
+            (todo) => todo !== todoThatShouldBeRemoved
+        );
+        return todoListWithoutTodoThatShouldBeRemoved;
+    }
+
+    function addTodo(todoList, todoThatShouldBeAdded) {
+        const lengthOfTodoList = todoList.length;
+        todoList[lengthOfTodoList] = todoThatShouldBeAdded;
         return todoList;
     }
 
     return (
-        <div className="border">
-            <div>Our ToDo List</div>
-            {todos.map((todo) => {
-                return (
-                    <div key={todo} className="flex space-x-8">
-                        <div>{todo}</div>
-                        <button
-                            onClick={(e) => {
-                                console.log("clicked");
-                                console.log(setTodos(removeTodo(todos, todo)));
-                                setTodos(removeTodo(todos, todo));
-                            }}
-                        >
-                            X
-                        </button>
-                    </div>
-                );
-            })}
-            <button onClick={() => setTodos([...todos, "another todo"])}>
-                Add another todo
-            </button>
+        <div className="flex flex-col items-center w-full py-8 border">
+            <h2 className="py-8 text-2xl text-center">Our ToDo List</h2>
+            <table className="mx-auto w-96">
+                <thead>
+                    <tr className="grid grid-cols-4 gap-4">
+                        <th>Due Date</th>
+                        <th>Todo</th>
+                        <th>Status</th>
+                        <th>Remove</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {todos.map((todo) => (
+                        <tr key={todo} className="grid grid-cols-4">
+                            <td>{todo.dueDate}</td>
+                            <td>{todo.name}</td>
+                            <td>{todo.status}</td>
+                            <td>
+                                <button
+                                    className="w-full"
+                                    onClick={() => {
+                                        setTodos(removeTodo(todos, todo));
+                                    }}
+                                >
+                                    X
+                                </button>
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+            <div className="flex py-8">
+                <input
+                    onChange={(event) => setNewTodoDate(event.target.value)}
+                    type="text"
+                    placeholder="Add Date"
+                />
+                <input
+                    onChange={(event) => setNewTodoName(event.target.value)}
+                    type="text"
+                    placeholder="Add new Date"
+                />
+                <select
+                    onChange={(event) => setNewTodoStatus(event.target.value)}
+                    value={newTodoStatus}
+                    placeholder="Select status"
+                >
+                    <option name="" id="">
+                        Not started
+                    </option>
+                    <option name="" id="">
+                        In progress
+                    </option>
+                    <option name="" id="">
+                        Done
+                    </option>
+                </select>
+                <button
+                    className="flex items-center justify-center gap-2 px-4 py-2 ml-4 border rounded-xl"
+                    onClick={() => {
+                        const newTodo = {
+                            name: newTodoName,
+                            dueDate: newTodoDate,
+                            status: newTodoStatus,
+                        };
+                        const newTodoList = addTodo(todos, newTodo);
+                        setTodos(newTodoList);
+                    }}
+                >
+                    <p>Add </p>
+                    <FiCheck />
+                </button>
+            </div>
             <pre className="py-20">
-                state is this: {JSON.stringify(todos, null, 2)}
+                state is this: {JSON.stringify(newTodoDate, null, 2)}
             </pre>
         </div>
     );
--- src/app/week-5/page.js
+++ src/app/week-5/page.js
@@ -6,22 +6,24 @@ import Wednesday910 from "./Wednesday910";
 export default function Page() {
     const [n, setN] = React.useState(0);
     return (
-        <div>
-            <h2>Try our Fibonacci function generating numbers up to n</h2>
-            <label>
-                Enter number of elements:
-                <input
-                    onChange={(e) => setN(parseInt(e.target.value))}
-                    type="text"
-                    placeholder=""
-                    className="bg-slate-200"
-                />
-            </label>
-            <pre>
-                {fibonacci(n).map((element) => {
-                    return `${element}, `;
-                })}
-            </pre>
+        <div className="">
+            <div className="py-8">
+                <h2>Try our Fibonacci function generating numbers up to n</h2>
+                <label>
+                    Enter number of elements:
+                    <input
+                        onChange={(e) => setN(parseInt(e.target.value))}
+                        type="text"
+                        placeholder=""
+                        className="bg-slate-200"
+                    />
+                </label>
+                <pre>
+                    {fibonacci(n).map((element) => {
+                        return `${element}, `;
+                    })}
+                </pre>
+            </div>
             <Wednesday910 />
         </div>
     );
